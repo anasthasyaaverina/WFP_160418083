fa178093f2e8d203e854271e55e0a193e64bd931
Anasthasya Averina <anasthasyaaverina@gmail.com>
2021-09-03 14:08:56 +0700

    add migration & empty seeder

---
+++ mybakery/database/migrations/2021_09_03_065558_add_stok_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStokColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('stok_produk')->after('harga_produk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('stok_produk');
+        });
+    }
+}
